d2c27bd6bdecfca830115edfa0db33dfdc76fcf3
266844938 <56297593+266844938@users.noreply.github.com>
2019-11-18 08:41:39 +0800

    Add files via upload

--- 11.181.c
+++ 11.181.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-int main (void)
+#include <stdio.h>
+int main(void)
 {
- int n,sum=0,term=1;
- printf("Entrr n:\n");
- scanf("%d",&n);
-  for(int i=1;i<=5;i++){
- term*=i;
-   sum+=term;
-  }
-   printf("sum is %d\n",sum);
-   return 0;
+    int n, sum = 0, term = 1;
+    printf("Entrr n:\n");
+    scanf("%d", &n);
+    for (int i = 1; i <= 5; i++)
+    {
+        term *= i;
+        sum += term;
+    }
+    printf("sum is %d\n", sum);
+    return 0;
 }
